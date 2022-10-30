1d69942d0d4ad4239e3dc13b2be5bbacd2f4b772
Smaranda Tasmoc <fo21366@it075749.wks.bris.ac.uk>
2022-10-30 11:38:25 +0000

    added game of life functions

--- gol/distributor.go
+++ gol/distributor.go
@@ -9,6 +9,90 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+func getLiveNeighbours(p Params, world [][]byte, a, b int) int {
+	var alive = 0
+	var widthLeft int
+	var widthRight int
+	var heightUp int
+	var heightDown int
+
+	if a == 0 {
+		widthLeft = p.ImageWidth - 1
+	} else {
+		widthLeft = a - 1
+	}
+	if a == p.ImageWidth-1 {
+		widthRight = 0
+	} else {
+		widthRight = a + 1
+	}
+
+	if b == 0 {
+		heightUp = p.ImageHeight - 1
+	} else {
+		heightUp = b - 1
+	}
+
+	if b == p.ImageHeight-1 {
+		heightDown = 0
+	} else {
+		heightDown = b + 1
+	}
+
+	if isAlive(world[widthLeft][b]) {
+		alive = alive + 1
+	}
+	if isAlive(world[widthRight][b]) {
+		alive = alive + 1
+	}
+	if isAlive(world[widthLeft][heightUp]) {
+		alive = alive + 1
+	}
+	if isAlive(world[a][heightUp]) {
+		alive = alive + 1
+	}
+	if isAlive(world[widthRight][heightUp]) {
+		alive = alive + 1
+	}
+	if isAlive(world[widthLeft][heightDown]) {
+		alive = alive + 1
+	}
+	if isAlive(world[a][heightDown]) {
+		alive = alive + 1
+	}
+	if isAlive(world[widthRight][heightDown]) {
+		alive = alive + 1
+	}
+	return alive
+}
+
+func isAlive(cell byte) bool {
+	if cell == 255 {
+		return true
+	}
+	return false
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageWidth)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageHeight)
+	}
+	for i := 0; i < p.ImageHeight; i++ {
+		for j := 0; j < p.ImageWidth; j++ {
+			neighbours := getLiveNeighbours(p, world, i, j)
+			if world[i][j] == 0xff && (neighbours < 2 || neighbours > 3) {
+				newWorld[i][j] = 0x0
+			} else if world[i][j] == 0x0 && neighbours == 3 {
+				newWorld[i][j] = 0xff
+			} else {
+				newWorld[i][j] = world[i][j]
+			}
+		}
+	}
+	return newWorld
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
